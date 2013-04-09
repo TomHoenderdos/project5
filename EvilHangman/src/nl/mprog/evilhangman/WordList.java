5e2f0c32a21bef6b21594bf7198be2aff43ea2c1
jeroen <jeroenpg@gmail.com>
2013-04-09 02:13:56 +0200

    poging tot raw words file

--- EvilHangman/src/nl/mprog/evilhangman/WordList.java
+++ EvilHangman/src/nl/mprog/evilhangman/WordList.java
@@ -1,5 +1,12 @@
 package nl.mprog.evilhangman;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Random;
 
@@ -15,7 +22,37 @@ public class WordList extends SparseArray<WordListItem>{
 	}
 	
 	public void parseWords(Context ctx) {
-		String[] words = ctx.getResources().getStringArray(R.array.word_list);
+ /*
+	    try {			
+	    	BufferedInputStream bis = new BufferedInputStream(ctx.getResources().openRawResource(R.raw.words));
+	    	BufferedReader reader = new BufferedReader(new InputStreamReader(bis));
+	    	
+	    	try {
+	    		String line = null; 
+
+				while (bis.available() > 0) {
+					reader.readLine();
+				}
+				
+	    		while (( line = input.readLine()) != null){    				
+    				int key = line.length();
+    				if(this.get(key) == null) {
+    					this.put(line.length(), new WordListItem());
+    				}
+    				this.get(key).add(line.toLowerCase());
+	    		}
+	    	}
+	    	finally {
+	    		bis.close();
+	    		dis.close();
+	    	}
+	    }
+	    catch (FileNotFoundException ex) {
+	    }
+	    catch (IOException ex){
+	    }
+*/
+		/*String[] words = ctx.getResources().getStringArray(R.array.word_list);
 		for(String word : words) {
 			int key = word.length();
 			if(this.get(key) == null) {
@@ -25,7 +62,7 @@ public class WordList extends SparseArray<WordListItem>{
 		}
 		for(int i = 0; i < this.size(); i++) {
 		   Collections.sort(this.valueAt(i));
-		}
+		}*/
 	}
 	
 	public String getRandomWord() {
